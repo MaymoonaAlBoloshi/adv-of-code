36ffd75051a6be0e4e337f9e5589af51a93d8afb
MaymoonaAlBoloshi <maymoona.al.boloshi@gmail.com>
2023-12-16 11:34:42 +0400

    d1 p1 ts 2023

---
+++ ts/d1-2023.ts
@@ -0,0 +1,30 @@
+console.log("hello");
+const fs = require("fs");
+
+const readFile = async () => {
+  return await fs.promises.readFile("./d1-2023.txt", "utf-8");
+};
+
+const main = async () => {
+  const text = await readFile();
+  
+  const dataArray = text.split("\n");
+
+  let totalNumber = 0;
+
+  for (const line of dataArray) {
+    const digits = line.match(/\d/g);
+
+    if (digits) {
+      const firstDigit = digits[0];
+      const lastDigit = digits[digits.length - 1];
+
+      const twoDigitNum = parseInt(firstDigit + lastDigit);
+      totalNumber += twoDigitNum;
+    }
+  }
+
+  console.log(totalNumber);
+};
+
+main();
